c03de4a6c32d5a33e22c56be983c3e7041401cc2
Vivek Raj <chinnujune@gmail.com>
2015-04-03 13:18:42 +0530

    Generate Unique link for sender

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -9,11 +9,21 @@ class EmailMailer < ApplicationMailer
 
       @senders.each do |sender|
         user = User.find(sender)
-        @random_key = SecureRandom.hex
+        @random_key = generate_unique_token(user.email)
 
         @shorten_link = Rails.application.secrets.sender_link + @random_key
         Sender.create(:email_id => email.id, :user_id => user.id, :unique_key => @random_key)
         mail(to: user.email, subject: 'Please answer the Question') #if @listener != user.username
+      end
+    end
+    
+    private
+
+    def generate_unique_token(email)
+      loop do
+        token = SecureRandom.hex
+        user = Sender.find_by_unique_key(token)
+        return token unless user
       end
     end
 
