62df661f439d870330f09a24ea679b891777c340
Vivek Raj <chinnujune@gmail.com>
2015-04-06 00:42:14 +0530

    Listener Link invalid handle

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,8 +1,13 @@
 class ResponseController < ApplicationController
   skip_before_filter :authenticate_user!
 
+  # Response to the Listener
   def listener_response
     @email = Email.where(:listener_link => params[:link]).first
+    if @email.nil?
+      flash[:notice] = "Invalid Token"
+      redirect_to sorry_path and return
+    end
 
     listener_id = Email.find(@email.id).listener_id
     @listener = EmailSender.where(:id => listener_id).first
